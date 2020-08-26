ae0cb121c46f0a20f27d4811b2ae3916716680e2
walterdane <walterdane1@icloud.com>
2020-08-26 01:09:44 -0500

    linked list, queue, and stack implementation

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,36 @@
-linked_list.py
+from collections import deque
+"""
+deque uses an implementation of a linked list in which you can
+access, insert, or remove elements from the beginning or end of a list
+with constant O(1) performance. To populate the linked list, pass in an
+iterable such as a string or list of objects
+"""
+deque(['jacob','matthew','bryan','mark'])
+
+"""
+Since a deque is a double-ended queue, it has the flexibility to
+act as a queue or a stack
+"""
+
+"""
+Queue Implementation: append elements to the right
+"""
+queue = deque()
+queue.append('martha')
+queue.append('susan')
+queue.append('cate')
+print(queue.popleft())
+print(queue.popleft())
+print(queue.popleft())
+
+"""
+Stack Implementation: append elements to the left
+"""
+queue = deque()
+queue.appendleft('plate1')
+queue.appendleft('plate2')
+queue.appendleft('plate3')
+print(queue.popleft())
+print(queue.popleft())
+print(queue.popleft())
+

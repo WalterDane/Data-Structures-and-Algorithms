ad705ff318041f090921d15dbc2e68ce03f61c15
WalterDane <walterdane1@icloud.com>
2020-09-21 10:43:14 -0500

    creation of Graph class

---
+++ graphy.py
@@ -0,0 +1,8 @@
+"""
+Formal Definition: A graph is a pair of sets (V, E) where V is the set of
+vertices and E is the set of edges, connecting the pairs of vertices
+"""
+
+class Graph():
+    def __init__(self):
+        pass
